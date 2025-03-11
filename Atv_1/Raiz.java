23133d2c06141e676d1ddc7308c2c2c658ee6d41
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-03-11 04:44:13 -0700

    Update Raiz.java

--- Atv_1/Raiz.java
+++ Atv_1/Raiz.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class bomdia {
     public static void main (String[] args) {
-      int a = 0;
-      int r = 0;
+      Double a = 0.00;
+      Double r = 0.00;
     Scanner scanner = new Scanner(System.in);
     System.out.println("Insira o valor da raiz: ");
-    r = scanner.nextInt();
-    a =  (r * r) * 314 / 100;
+    r = scanner.nextDouble();
+     a =  Math.pow(r, 2) * Math.PI;
     System.out.println(a);
   }
 }
