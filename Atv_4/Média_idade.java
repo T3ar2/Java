083b1471e2cf2a0e887261221b41a2ce16441ac6
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-03-14 16:08:20 -0700

    Update Média_idade.java

--- Atv_4/Média_idade.java
+++ Atv_4/Média_idade.java
@@ -6,22 +6,29 @@ public class Main {
     public static void main(String[] args){
 
         Scanner teclado = new Scanner(System.in);
-        int idade1 = -1;
-        int idade2 = -1;
-        while (idade1<0 || idade1> 150){
-          System.out.println("Insira a primeira idade: ");
-          idade1 = teclado.nextInt();
-        }
-        
-        
-        while (idade2<0 || idade2> 150){
-        System.out.println("\nInsira a segunda idade: ");
-        idade2 = teclado.nextInt();
-        }
-        
-        int media = (idade1 + idade2) / 2;
-        
-        System.out.println("A média é " + media);
-    }
 
+        int idade = 0;
+        int idade_soma = 0;
+
+          System.out.println("\nInsira a quantidade de idades que você vai inserir (Máximo 50): ");
+          int a = teclado.nextInt();
+
+          
+            
+            for ( int x = 0; x < a ; x++){
+              if (a > 50){
+                System.out.println("Quantidade inválida!");
+                break;
+              }
+              System.out.println("\nInsira a idade: ");
+              idade = teclado.nextInt();
+              idade_soma += idade;
+            }
+        if (idade > 0){
+          int media = idade_soma / a;
+          System.out.println("A média é " + media);
+        }
+
+    }
+          
 }
