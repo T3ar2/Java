b2233fce725ea1d5259edfac7de86d791ecd2fa5
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-03-31 09:41:15 -0700

    Create JogadorFutebol.java

---
+++ Atv_7/JogadorFutebol.java
@@ -0,0 +1,19 @@
+public class JogadorFutebol extends Pessoa {
+    private String posicao;
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public void Treino() {
+        System.out.println("O Jogador " + getName() + " (" + getPosicao()+ ") " + " está Treinando");
+    }
+
+    public void Jogar() {
+        System.out.println("O Jogador " + getName() + " (" + getPosicao()+ ") " + " está Jogando");
+    }
+}
