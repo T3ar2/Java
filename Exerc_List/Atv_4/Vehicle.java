3ed69cc94ed01f8738020c1640cbcf789e4743e7
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-02 10:07:08 -0700

    Update Vehicle.java

--- Exerc_List/Atv_4/Vehicle.java
+++ Exerc_List/Atv_4/Vehicle.java
@@ -6,11 +6,14 @@ public class Vehicle {
     private String brand;
     private int max_speed;
     private int car_speed;
+    private int car_year;
 
-    Vehicle( String brand, String model,int max_speed){
+    Vehicle( String brand, String model,int max_speed, int car_year){
         this.brand = brand;
         this.model = model;
         this.max_speed = max_speed;
+        this.car_year = car_year;
+
     }
 
 
@@ -27,6 +30,10 @@ public class Vehicle {
         return model;
     }
 
+    public int getCar_year() {
+        return car_year;
+    }
+
     public String getBrand() {
         return brand;
     }
@@ -36,6 +43,7 @@ public class Vehicle {
     }
     void getSettings(){
         System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getCar_year());
         System.out.println("Brand: " + getBrand());
         System.out.println("Speed: " + getCar_speed() + " km/h ");
     }
