e46b6b23901dd8dc93c12d60aff988fff649cd0f
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-02 08:16:24 -0700

    Create Main.java

---
+++ Exerc_List/Atv_3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main (String[] args){
+        Library library = new Library("Maus: A Survivor's Tale", "Art Spiegelman", 160);
+        
+        library.Details();
+    }
+}
