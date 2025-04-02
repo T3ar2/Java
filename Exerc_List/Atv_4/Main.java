5afe16834b76137f31a0b45c485a43db094801a6
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-02 09:32:54 -0700

    Create Main.java

---
+++ Exerc_List/Atv_4/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//04) Uma concessionária deseja armazenar informações sobre seus veículos. Cada veículo tem um
+//modelo, uma marca e uma velocidade máxima. A velocidade do veículo pode ser alterada, mas não
+//pode ultrapassar sua velocidade máxima.
+//Requisito: Implemente a classe Veiculo garantindo que a velocidade não ultrapasse o limite
+//máximo.
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Vehicle vehicle = new Vehicle("toyota", "toyota supra mk4", 250);
+
+        vehicle.getSettings();
+
+        System.out.println("What's the speed for this car (in number)? ");
+        vehicle.setCar_speed(scanner.nextInt());
+
+        if ((vehicle.getCar_speed() <= vehicle.getMax_speed()) & (vehicle.getCar_speed() >= 0 )){
+            vehicle.getSettings();
+        }
+        else {
+            System.out.println("You can't, insert this kind a number ");
+        }
+    }
+}
