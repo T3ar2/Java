02609d160d4351321ce8027648d4a087b58b3753
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-02 09:29:29 -0700

    Update Main.java

--- Exerc_List/Atv_3/Main.java
+++ Exerc_List/Atv_3/Main.java
@@ -1,3 +1,7 @@
+
+03) Uma biblioteca deseja cadastrar seus livros. Cada livro deve ter um título, autor e número de
+//páginas. O número de páginas não pode ser alterado após a criação do livro.
+// Requisito: Crie a classe Livro garantindo que o número de páginas seja imutável após a criação
 public class Main {
 
     public static void main (String[] args){
