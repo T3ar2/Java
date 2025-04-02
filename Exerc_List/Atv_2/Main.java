0d54f8542492e5fd69384037930ab8fd989d65d4
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-02 09:28:41 -0700

    Update Main.java

--- Exerc_List/Atv_2/Main.java
+++ Exerc_List/Atv_2/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+//02) Uma empresa precisa de um sistema para gerenciar seus funcionários. Cada funcionário tem um
+//nome, cargo e salário. O salário pode ser atualizado apenas através de um aumento percentual.
+//Requisito: Crie a classe Funcionario garantindo que o salário seja atualizado apenas por um
+//método apropriado.
+
 public class Main{
 
     public static void main (String[] args){
