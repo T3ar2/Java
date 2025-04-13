f6f2fa42293b5f657d626203ed48aa6c0b2f07be
T3ar2 <165920643+T3ar2@users.noreply.github.com>
2025-04-13 04:48:43 -0700

    Create Estufa.java

---
+++ List_exerc2/Atv_3/Estufa.java
@@ -0,0 +1,13 @@
+public class Estufa {
+    private String id;
+
+
+    Estufa(String id){
+        this.id = id;
+    }
+
+    public void lerValor(){
+    System.out.println("O sensor " + id + " escaneou : " );
+    }
+
+}
